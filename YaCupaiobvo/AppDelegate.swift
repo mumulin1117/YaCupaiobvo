454422850b0979a7218be071cda946c78ed16634
mumulin1117 <mumulin1117@163.com>
2026-01-20 11:00:29 +0800

    Initial commit

---
+++ YaCupaiobvo/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  YaCupaiobvo
+//
+//  Created by mumu on 2026/1/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+
+
+}
+
